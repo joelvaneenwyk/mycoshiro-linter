9e4c5e846788f89ff55c1a3eb04eb705773e743c
joelvaneenwyk <joel.vaneenwyk@gmail.com>
2023-08-13 13:55:59 -0700

    Switch babel to typescript

--- babel.config.ts
+++ babel.config.ts
@@ -1,13 +1,15 @@
-// babel.config.js
-module.exports = {
+// babel.config.ts
+const config = {
   presets: [
-    ['@babel/preset-env', {targets: {node: 'current'}}],
+    ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
   ],
   plugins: [
-    ['@babel/plugin-proposal-decorators', {version: 'legacy'}],
+    ['@babel/plugin-proposal-decorators', { version: 'legacy' }],
     ['@babel/plugin-proposal-class-properties'],
     ['@babel/plugin-proposal-private-methods'],
     ['import-glob'],
   ],
 };
+
+export default config;
