f9488618044142f1baca14a23e5f3506e16fa700
joelvaneenwyk <joel.vaneenwyk@gmail.com>
2023-08-13 13:26:50 -0700

    Initial pass at fixing tests

--- jest.config.ts
+++ jest.config.ts
@@ -2,20 +2,19 @@
 // jest.config.ts
 //
 
-import { Config } from '@jest/types';
+import {Config} from '@jest/types';
 
 process.env.TZ = 'UTC'; // eslint-disable-line no-undef
 
-// Sync object
-export default async (): Promise<Config.InitialOptions> => {
-    return {
-        transformIgnorePatterns: [
-            '"/node_modules/(?!unified-lint-rule)',
-        ],
-        testMatch: [
-            '**/__tests__/**/*.[jt]s?(x)',
-            '**/?(*.)+(spec|test).[jt]s?(x)',
-            '!**/__tests__/common.ts',
-        ]
-    };
-}
+const config: Config.InitialOptions = {
+  transformIgnorePatterns: [
+    '"/node_modules/(?!unified-lint-rule)',
+  ],
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+    '!**/__tests__/common.ts',
+  ]
+};
+
+export default config;
